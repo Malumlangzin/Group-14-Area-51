cbd004713aaa90838fc632d5f0cfc7dbbc5af577
Malumlangzin <langandzunga06@gmail.com>
2025-10-20 15:39:27 +0200

    3D meshes

--- Area 51/Assets/Scripts/FPController.cs
+++ Area 51/Assets/Scripts/FPController.cs
@@ -43,12 +43,15 @@ public class FPController : MonoBehaviour
     public float throwForce = 10f;
     public float throwUpwardBoost = 2f;
 
+    [Header("Animation Settings")]
+    [Space(5)]
+    public Animator animator;
+
     private CharacterController controller;
     private Vector2 moveInput;
     private Vector2 lookInput;
     private Vector3 velocity;
     private float verticalRotation = 0f;
-
 
     private void Awake()
     {
@@ -214,6 +217,9 @@ public class FPController : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+        float movementMagnitude = new Vector2(moveInput.x, moveInput.y).magnitude;
+        animator.SetFloat("Speed", movementMagnitude);
     }
 
     public void HandleLook()
@@ -253,6 +259,9 @@ public class FPController : MonoBehaviour
         {
             currentSpeed = moveSpeed;
         }
+
+        float movementMagnitude = new Vector2(moveInput.x, moveInput.y).magnitude;
+        animator.SetFloat("Speed", movementMagnitude);
     }
 
     public void HandleJump()
