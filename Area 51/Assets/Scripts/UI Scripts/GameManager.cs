609e67658a147b8ed99144071fbaeb50e7608a16
loml-sedi <lightofmylife.sedi@gmail.com>
2025-10-24 00:20:27 +0200

    Update GameManager.cs

--- Area 51/Assets/Scripts/UI Scripts/GameManager.cs
+++ Area 51/Assets/Scripts/UI Scripts/GameManager.cs
@@ -1,109 +1,114 @@
-using UnityEditor;
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine.UI;
+using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager3D : MonoBehaviour
 {
+    [Header("Puzzle Settings")]
     [SerializeField] private Transform gameTransform;
     [SerializeField] private Transform piecePrefab;
+    [SerializeField] private int size = 3;
+    [SerializeField] private float gapThickness = 0.01f;
 
-    private List<Transform> pieces;
+    private List<Transform> pieces = new List<Transform>();
     private int emptyLocation;
-    private int size;
     private bool shuffling = false;
 
-    private void CreateGamePieces(float gapThickness)
+    void Start()
     {
-        float width = 1 / (float)size; 
+        CreateGamePieces();
+    }
+
+    private void CreateGamePieces()
+    {
+        float width = 1f / size;
+
         for (int row = 0; row < size; row++)
         {
             for (int col = 0; col < size; col++)
             {
                 Transform piece = Instantiate(piecePrefab, gameTransform);
-                piece.localPosition = new Vector3(-1 + (2 * width * col) + width, +1 - (2 * width * row) - width, 0);
-
                 piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
+
+                piece.localPosition = new Vector3(
+                    -1 + (2 * width * col) + width,
+                    0,
+                    1 - (2 * width * row) - width
+                );
+
                 piece.name = $"{(row * size) + col}";
+
                 if ((row == size - 1) && (col == size - 1))
                 {
                     emptyLocation = (size * size) - 1;
                     piece.gameObject.SetActive(false);
                 }
-                else
-                {
-                    float  gap =gapThickness / 2;
-                    Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
-                    Vector2[] uv = new Vector2[4];
-                    uv[0] = new Vector2((width * col) + gap, 1 - (width * (row + 1)) - gap);
-                    uv[1] = new Vector2((width * (col + 1)) + gap, 1 - ((width * (row + 1))  - gap));
-                    uv[2] = new Vector2((width * col) + gap, 1 - ((width * row) + gap));
-                    uv[3] = new Vector2((width * (col + 1)) - gap, 1 - ((width * row) + gap));
 
-                    mesh.uv = uv;
-                }
+                Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
+                Vector2[] uv = new Vector2[4];
+                float gap = gapThickness / 2;
+
+                uv[0] = new Vector2((width * col) + gap, 1 - (width * (row + 1)) - gap);
+                uv[1] = new Vector2((width * (col + 1)) - gap, 1 - (width * (row + 1)) - gap);
+                uv[2] = new Vector2((width * col) + gap, 1 - (width * row) + gap);
+                uv[3] = new Vector2((width * (col + 1)) - gap, 1 - (width * row) + gap);
+
+                mesh.uv = uv;
+
+                if (!piece.GetComponent<Collider>())
+                    piece.gameObject.AddComponent<BoxCollider>();
+
+                pieces.Add(piece);
             }
         }
     }
 
-
-    void Start()
-    {
-        pieces = new List<Transform>();
-        size = 3;
-        CreateGamePieces(0.01f);
-    }
-
-
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 for (int i = 0; i < pieces.Count; i++)
                 {
                     if (pieces[i] == hit.transform)
                     {
-                        if (SwapIfValid(i, -size, size)) { break; }
-                        if (SwapIfValid(i, +size,size)) { break; }
-                        if (SwapIfValid(i, -1, 0)) { break; }
-                        if (SwapIfValid(i, +1, size - 1)) { break; }
+                        if (SwapIfValid(i, -size, size)) break;
+                        if (SwapIfValid(i, +size, size)) break;
+                        if (SwapIfValid(i, -1, 0)) break;
+                        if (SwapIfValid(i, +1, size - 1)) break;
                     }
                 }
             }
-        } 
-      
-        
+        }
+
         if (!shuffling && CheckCompletion())
         {
             shuffling = true;
             StartCoroutine(WaitShuffle(0.5f));
         }
-       
     }
 
     private bool SwapIfValid(int i, int offset, int colCheck)
     {
-        if (((i % size) != colCheck) && ((i + offset) == emptyLocation)){
+        if (((i % size) != colCheck) && ((i + offset) == emptyLocation))
+        {
             (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
-            (pieces[i].localPosition, pieces[i + offset].localPosition) = ((pieces[ i + offset].localPosition, pieces[i].localPosition));
+            (pieces[i].localPosition, pieces[i + offset].localPosition) =
+                (pieces[i + offset].localPosition, pieces[i].localPosition);
+
             emptyLocation = i;
             return true;
         }
         return false;
     }
+
     private bool CheckCompletion()
     {
         for (int i = 0; i < pieces.Count; i++)
         {
-            if (pieces[i].name != $"{i}")
-            {
-                return false;
-            }  
+            if (pieces[i].name != $"{i}") return false;
         }
         return true;
     }
@@ -119,26 +124,17 @@ public class GameManager : MonoBehaviour
     {
         int count = 0;
         int last = 0;
-        while (count < (size * size * size))
-        {
-            int rnd = Random.Range(0 , size *  size);
 
-            if (rnd == last) { continue; }
+        while (count < size * size * size)
+        {
+            int rnd = Random.Range(0, size * size);
+            if (rnd == last) continue;
             last = emptyLocation;
 
-            if (SwapIfValid(rnd, -size, size))
-            {
-                count++;
-            } else if (SwapIfValid(rnd, +size, size))
-            {
-                count++;
-            } else if (SwapIfValid(rnd, -1, 0))
-            {
-                count++;
-            } else if (SwapIfValid(rnd, +1, size - 1))
-            {
-                count++; 
-            }
+            if (SwapIfValid(rnd, -size, size)) count++;
+            else if (SwapIfValid(rnd, +size, size)) count++;
+            else if (SwapIfValid(rnd, -1, 0)) count++;
+            else if (SwapIfValid(rnd, +1, size - 1)) count++;
         }
     }
 }
