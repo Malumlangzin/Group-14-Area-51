003012e3a9e52da98c0e054f036826231445d5dd
Malumlangzin <langandzunga06@gmail.com>
2025-11-09 17:14:50 +0200

    game

--- Area 51/Assets/Scripts/Audio/VolumeManager.cs
+++ Area 51/Assets/Scripts/Audio/VolumeManager.cs
@@ -6,21 +6,27 @@ public class VolumeManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider MasterSlider;
 
     public void SetMasterVolume(float level)
     {
+        float voulme = MasterSlider.value;
+       
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(level) * 20);
     }
 
 
     public void SetMusicVolume(float level)
-    {
+    { 
+        float voulme = musicSlider.value;
+       
         audioMixer.SetFloat("Music", Mathf.Log10(level) * 20);
     }
 
 
     public void SetSoundFXVolume(float level)
-    {
+    { 
+        float voulme = musicSlider.value;
         audioMixer.SetFloat("SFX", Mathf.Log10(level) * 20);
     }
 }
